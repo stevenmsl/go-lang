e66c39116c502a69b324db566406031b83901895
Steven Lin <slin@merchantadvancecap.com>
2018-05-24 11:16:16 -0700

    Add functions to main.go

--- src/blockchain/main.go
+++ src/blockchain/main.go
@@ -12,12 +12,13 @@ You will also see a file named settings.json created under .vscode folder if thi
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"time"
 	/*	"encoding/json"
 		"io"
 		"log"
 		"net/http"
 		"os"
-		"time"
+
 
 		"github.com/davecgh/go-spew/spew"
 		"github.com/gorilla/mux"
@@ -36,12 +37,40 @@ type Block struct {
 //Blockchain ...
 var Blockchain []Block
 
+func generateBlock(oldBlock Block, BPM int) (Block, error) {
+	var newBlock Block
+	t := time.Now()
+	newBlock.Index = oldBlock.Index + 1
+	newBlock.Timestamp = t.String()
+	newBlock.BPM = BPM
+	newBlock.PrevHash = oldBlock.Hash
+	newBlock.Hash = calculateHash(newBlock)
+	return newBlock, nil
+}
+
 func calculateHash(block Block) string {
 	record := string(block.Index) + block.Timestamp + string(block.BPM) + block.PrevHash
 	h := sha256.New()
 	h.Write([]byte(record))
 	hased := h.Sum(nil)
 	return hex.EncodeToString(hased)
+}
+
+func isBlockValid(newBlock Block, oldBlock Block) bool {
+	if oldBlock.Index+1 != newBlock.Index {
+		return false
+	}
+
+	if oldBlock.Hash != newBlock.PrevHash {
+		return false
+	}
+
+	if calculateHash(newBlock) != newBlock.Hash {
+		return false
+	}
+
+	return true
+
 }
 
 func main() {
