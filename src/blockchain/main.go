875f7d9abe6501742b76bdb66d35a299e2e1a849
Steven Lin <slin@merchantadvancecap.com>
2018-05-29 10:10:49 -0700

    Add .env to the blockchain package folder

--- src/blockchain/main.go
+++ src/blockchain/main.go
@@ -1,6 +1,15 @@
 package main
 
 /*
+The .env file needs to be in the package folder (src\blockchain) so it can be located when you start debugging.
+*/
+
+/*
+Set the breakpoints before you start debugging.
+*/
+
+/*
+The .env file needs to be in the package folder (src\blockchain) so it can be located when you start debugging.
 Packages imported but not used will be deleted when you save the file if go.formatTool in user settings   is configured to use goreturns. Go to File\Preferences\Settings and put the following in the workspace settings to overwrite:
 {
     "go.formatTool": "gofmt"
