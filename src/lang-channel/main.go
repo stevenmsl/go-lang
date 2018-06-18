d81da900d2f91cb2ad5b968c95744ca78ba9d9e9
Steven Lin <slin@merchantadvancecap.com>
2018-06-18 11:33:42 -0700

    Add runSum function

--- src/lang-channel/main.go
+++ src/lang-channel/main.go
@@ -10,6 +10,31 @@ import (
 )
 
 func main() {
+	runSum()
+	buyItems()
+}
+
+func runSum() {
+	fmt.Println("In runSum...")
+	input := []int{1, 2, 3, 4, 5, 6}
+	c := make(chan int)
+	go sum(input[:len(input)/2], c)
+	go sum(input[len(input)/2:], c)
+	x, y := <-c, <-c
+	fmt.Printf("x: %v, y: %v, x+y: %v\n", x, y, x+y)
+}
+func sum(input []int, c chan int) {
+	fmt.Printf("Sum up %v\n", input)
+	sum := 0
+	for _, v := range input {
+		sum += v
+	}
+	c <- sum
+	fmt.Printf("result %v pushed to channel\n", sum)
+}
+
+func buyItems() {
+	fmt.Println("In buyItems...")
 	out := make(chan string)
 	in := make(chan string)
 	go buy(in, out, 1)
@@ -26,7 +51,6 @@ func main() {
 	fmt.Println(<-out)
 	fmt.Println(<-out)
 	fmt.Println(<-out)
-
 }
 
 //<-chan string means you can only put stuff into the channel
