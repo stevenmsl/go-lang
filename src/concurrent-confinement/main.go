8d3c12f8bb61926e0a4aa74a9bb7856f15d619b3
Steven Lin <slin@merchantadvancecap.com>
2018-06-25 13:11:28 -0700

    Add printString function

--- src/concurrent-confinement/main.go
+++ src/concurrent-confinement/main.go
@@ -6,10 +6,13 @@ data and concurrency primitives for multiple concurrent processes to use.
 */
 
 import (
+	"bytes"
 	"fmt"
+	"sync"
 )
 
 func main() {
+	printString("golang")
 	results := chanOwner()
 	consumer(results)
 }
@@ -32,4 +35,26 @@ func consumer(results <-chan int) {
 		fmt.Printf("Received: %d\n", result)
 	}
 	fmt.Println("Done receiving!")
+}
+
+func printData(wg *sync.WaitGroup, data []byte) {
+	defer wg.Done()
+	var buff bytes.Buffer
+	for _, b := range data {
+		fmt.Fprintf(&buff, "%c", b)
+	}
+	fmt.Println(buff.String())
+}
+
+func printString(value string) {
+	fmt.Println("In printString...")
+	var wg sync.WaitGroup //WaitGroup waits for a collection of goroutines to finish
+	wg.Add(2)
+	data := []byte(value)
+	//Constrain the go routines to only work on certain the part of the slice.
+	//Using lexical scope to avoid the need of implementing synchronization
+	//the program will be simpler, and the performance would be better.
+	go printData(&wg, data[:3])
+	go printData(&wg, data[3:])
+	wg.Wait() //"Wait" blocks until the WaitGroup counter is zero.
 }
