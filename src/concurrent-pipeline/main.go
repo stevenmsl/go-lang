bff2d5a8241d53d2bc872287e3dd7750eaf92573
Steven Lin <slin@merchantadvancecap.com>
2018-07-06 16:45:19 -0700

    Add runPipelineC function

--- src/concurrent-pipeline/main.go
+++ src/concurrent-pipeline/main.go
@@ -24,13 +24,80 @@ import (
 )
 
 func main() {
-	runPipelineS()
+	runPipelineC()
+	//runPipelineS()
 	//runPipelineBP()
+}
+
+func runPipelineC() {
+	done := make(chan interface{})
+	defer close(done)
+	intStream := generator(done, 1, 2, 3, 4)
+	//each stage of the pipeline is executing concurrently
+	pipeline := multiplyC(done, addC(done, multiplyC(done, intStream, 2), 1), 2)
+	for v := range pipeline {
+		fmt.Println(v)
+	}
+}
+
+//t huhe generator function converts a discrete set of values into a stream of data on a channel
+func generator(done <-chan interface{}, integers ...int) <-chan int {
+	intStream := make(chan int)
+	go func() {
+		defer close(intStream)
+		for _, i := range integers {
+			select {
+			case <-done:
+				return
+			case intStream <- i:
+			}
+		}
+	}()
+	return intStream
+}
+
+func multiplyC(
+	done <-chan interface{},
+	intStream <-chan int,
+	multiplier int,
+) <-chan int {
+	multipliedStream := make(chan int)
+	go func() {
+		defer close(multipliedStream)
+		for i := range intStream {
+			select {
+			case <-done:
+				return
+			case multipliedStream <- i * multiplier:
+			}
+		}
+	}()
+	return multipliedStream
+}
+
+func addC(
+	done <-chan interface{},
+	intStream <-chan int,
+	additive int,
+) <-chan int {
+	addedStream := make(chan int)
+	go func() {
+		defer close(addedStream)
+		for i := range intStream {
+			select {
+			case <-done:
+				return
+			case addedStream <- i + additive:
+			}
+		}
+	}()
+	return addedStream
 }
 
 func runPipelineS() {
 	ints := []int{1, 2, 3, 4}
 	for _, v := range ints {
+		//We instantiate the pipeline for each iteration: 4 times in this case
 		fmt.Println(multiplyS(addS(multiplyS(v, 2), 1), 2))
 	}
 }
