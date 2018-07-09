ea60acac9842207a9c430634bd9f4420528fee7d
Steven Lin <slin@merchantadvancecap.com>
2018-07-09 12:48:34 -0700

    Modify runPipelineC

--- src/concurrent-pipeline/main.go
+++ src/concurrent-pipeline/main.go
@@ -29,14 +29,32 @@ func main() {
 	//runPipelineBP()
 }
 
+/*
+When you look at the output, it looks quite random:
+Generated the number 1
+1 + 2 = 2 done by stage Added by 2
+3 x 3 = 9 done by stage Multiplied by 3
+1 x 1 = 1 done by stage Multiplied by 1
+
+In the above example, it appears to be in iteration 0 stage “Added by 2” finished before stage “Multiplied by 1”,
+which it’s not possible as the later stage “Added by 2” is depending on the outcome of the previous stage “Multiplied by 1”.
+I think the reason for this inconsistence is due to the time needed between you pushed the result to the channel
+and you printed the result.
+Don’t forget all stages are executing concurrently, and the next stage probably already finished
+by the time you finished printing the result in the current stage.
+
+*/
 func runPipelineC() {
 	done := make(chan interface{})
 	defer close(done)
 	intStream := generator(done, 1, 2, 3, 4)
 	//each stage of the pipeline is executing concurrently
-	pipeline := multiplyC(done, addC(done, multiplyC(done, intStream, 2), 1), 2)
+	pipeline := multiplyC(done, addC(done, multiplyC(done, intStream, 1), 2), 3)
+	index := 0
+	//If the range expression is a channel, at most one iteration variable is permitted.
 	for v := range pipeline {
-		fmt.Println(v)
+		fmt.Printf("Iteration %v Completed. Produced the number %v\n", index, v)
+		index++
 	}
 }
 
@@ -44,12 +62,14 @@ func runPipelineC() {
 func generator(done <-chan interface{}, integers ...int) <-chan int {
 	intStream := make(chan int)
 	go func() {
+		defer fmt.Println("Generator closed")
 		defer close(intStream)
 		for _, i := range integers {
 			select {
 			case <-done:
 				return
 			case intStream <- i:
+				fmt.Printf("Generated the number %v\n", i)
 			}
 		}
 	}()
@@ -63,12 +83,15 @@ func multiplyC(
 ) <-chan int {
 	multipliedStream := make(chan int)
 	go func() {
+		defer fmt.Printf("stage Multiplied by %v closed\n", multiplier)
 		defer close(multipliedStream)
 		for i := range intStream {
 			select {
 			case <-done:
 				return
 			case multipliedStream <- i * multiplier:
+				fmt.Printf("%v x %v = %v done by stage Multiplied by %v \n",
+					i, multiplier, i*multiplier, multiplier)
 			}
 		}
 	}()
@@ -82,12 +105,15 @@ func addC(
 ) <-chan int {
 	addedStream := make(chan int)
 	go func() {
+		defer fmt.Printf("stage Added by %v closed\n", additive)
 		defer close(addedStream)
 		for i := range intStream {
 			select {
 			case <-done:
 				return
 			case addedStream <- i + additive:
+				fmt.Printf("%v + %v = %v done by stage Added by %v \n",
+					i, additive, i*additive, additive)
 			}
 		}
 	}()
