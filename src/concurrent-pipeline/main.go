f2db03c884feec99bf360c847f44af9e459923d5
Steven Lin <slin@merchantadvancecap.com>
2018-07-10 11:39:47 -0700

    Add runPipelineTake function

--- src/concurrent-pipeline/main.go
+++ src/concurrent-pipeline/main.go
@@ -24,9 +24,61 @@ import (
 )
 
 func main() {
-	runPipelineC()
+	runPipelineTake()
+	//runPipelineC()
 	//runPipelineS()
 	//runPipelineBP()
+}
+
+func runPipelineTake() {
+	done := make(chan interface{})
+	defer close(done)
+	for num := range take(done, repeat(done, 1, 2), 10) {
+		fmt.Printf("%v ", num)
+	}
+	fmt.Println()
+}
+
+//stage: generate a stream of data
+func repeat(
+	done <-chan interface{},
+	values ...interface{},
+) <-chan interface{} {
+	valueStream := make(chan interface{})
+	go func() {
+		defer fmt.Println("repeatGenerator closed")
+		defer close(valueStream)
+		for {
+			for _, v := range values {
+				select {
+				case <-done:
+					return
+				case valueStream <- v:
+				}
+			}
+		}
+	}()
+	return valueStream
+}
+
+//stage: limit the pipeline
+func take(
+	done <-chan interface{},
+	valueStream <-chan interface{},
+	num int,
+) <-chan interface{} {
+	takeStream := make(chan interface{})
+	go func() {
+		defer close(takeStream)
+		for i := 0; i < num; i++ {
+			select {
+			case <-done:
+				return
+			case takeStream <- <-valueStream:
+			}
+		}
+	}()
+	return takeStream
 }
 
 /*
